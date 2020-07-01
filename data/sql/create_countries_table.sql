c4e442963226630f951bf784f09bab796076197a
elarsaks <elarsaks@gmail.com>
2020-07-01 13:49:14 +0300

    Clean up docker-compose file andremove type from sql.

--- data/sql/create_countries_table.sql
+++ data/sql/create_countries_table.sql
@@ -2,5 +2,5 @@ create table countries (
 	country_code varchar(10) not null primary key,
 	country_name varchar(124),
 	region varchar(124),
-	income_group varchar(124),
+	income_group varchar(124)
 );
